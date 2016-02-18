4a19be61bbeca7788eb14d19b74c3fced6fd68fe
Karthick <rkarthick31996@gmail.com>
2016-02-18 19:28:53 +0530

    BUG-FIXES

--- displayorder.php
+++ displayorder.php
@@ -17,6 +17,11 @@ echo " 		<html>
 			  <link rel=\"stylesheet\" href=\"http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css\">
 			  <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js\"></script>
 			  <script src=\"http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js\"></script>
+			   <style>
+				        body{
+				    font-family: 'Roboto', sans-serif;
+				}
+ 				 </style>
 			</head>
 			<body>";
 echo "<div class=\"alert alert-info\">
